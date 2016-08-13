b827f9bea1a9023724dddddfd5144ab0ffe0c792
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2016-08-13 14:10:24 -0700

    Send the password, output goes active.

--- sim/test_program.sv
+++ sim/test_program.sv
@@ -6,14 +6,47 @@
 
 `define IN sim_top.tb.test_core_inst_in_bfm
 `define OUT sim_top.tb.test_core_inst_out_bfm
-`define MGMT sim_top.tb.test_core_inst_mgmt_bfmt
+`define MGMT sim_top.tb.test_core_inst_mgmt_bfm
 module test_program;
 
   initial begin
-    wait(~`RESET);
+    fork
+      begin
+        init();
+        wait(~`RESET);
+        repeat(2) @(posedge `CLK);
+
+        mgmt_send();
+
+        in_send(16'h4949);
+        in_send(16'hb5de);
+        in_send(16'h9600);
+
+      end
+    join_any
+
     repeat(10) @(posedge `CLK);
     $stop;
   end
+
+  task automatic init;
+    `IN.init();
+    `MGMT.init();
+    `OUT.init();
+  endtask
+
+  task automatic mgmt_send;
+    `MGMT.set_transaction_data(8'hX);
+    `MGMT.push_transaction();
+
+    wait(`MGMT.signal_src_transaction_complete);
+    @(posedge `CLK);
+  endtask
+
+  task automatic in_send(input logic [15:0] data);
+    `IN.set_transaction_data(data);
+    `IN.push_transaction();
+  endtask
 
 endmodule
 
