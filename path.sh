e5ef19a9faa3658b0b04204bef682382fa037a6b
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2018-11-03 15:47:28 -0700

    Env variable assignment for eclipse; path to to nios2 tools.

--- path.sh
+++ path.sh
@@ -1,1 +1,3 @@
-export PATH=$PATH:$QSYS_ROOTDIR:$QSYS_ROOTDIR/../../../modelsim_ase/linuxaloem:$QSYS_ROOTDIR/../../bin
+# without this, eclipse segfaults with no error
+export SWT_GTK3=0
+export PATH=$PATH:$QSYS_ROOTDIR:$QSYS_ROOTDIR/../../../modelsim_ase/linuxaloem:$QSYS_ROOTDIR/../../bin:$QSYS_ROOTDIR/../../../nios2eds/bin
